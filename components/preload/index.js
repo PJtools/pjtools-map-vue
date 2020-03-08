261ef522307b643460803925abb03d7666d8170e
pjtools <pjtools@vip.qq.com>
2020-03-08 23:51:06 +0800

    调整Preload预加载组件为队列管理方式；

--- components/preload/index.js
+++ components/preload/index.js
@@ -6,6 +6,7 @@
 
 import createjs from 'createjs-npm/lib/preload';
 import merge from 'lodash/merge';
+import PQueue from 'p-queue';
 
 // 定义默认的预加载对象的参数选项
 const DEFAULT_PRELOAD_OPTIONS = {
@@ -32,7 +33,7 @@ const fetchManifestList = function(baseUrl = DEFAULT_PRELOAD_OPTIONS.baseUrl) {
       if (result && typeof result === 'object' && result.load) {
         // 处理Manifest配置的别名
         const paths = result.paths || {};
-        const outExports = result.exports || {};
+        const $exports = result.exports || {};
         const alias = {};
         Object.keys(paths).map(key => {
           alias[key] = paths[key];
@@ -48,7 +49,7 @@ const fetchManifestList = function(baseUrl = DEFAULT_PRELOAD_OPTIONS.baseUrl) {
         const jsList = (result && result.load && result.load.js) || [];
         const filesJs = [];
         jsList.map(item => {
-          filesJs.push({ id: item, src: api.$alias[item] || item, type: createjs.AbstractLoader.JAVASCRIPT, exports: outExports[item] || null });
+          filesJs.push({ id: item, src: api.$alias[item] || item, type: createjs.AbstractLoader.JAVASCRIPT, exports: $exports[item] || null });
         });
         // 注销实例对象
         instance.destroy();
@@ -70,7 +71,7 @@ const fetchManifestList = function(baseUrl = DEFAULT_PRELOAD_OPTIONS.baseUrl) {
  * 根据静态的Manifest数据转换成指定的对应的JSON数据项
  * @param {Array} manifest 待处理的Manifest数据集合
  */
-const handleStaticManifestList = manifest => {
+const handleStaticManifestList = function(manifest) {
   return new Promise(resolve => {
     const filesCss = [];
     const filesJs = [];
@@ -116,9 +117,111 @@ const handleStaticManifestList = manifest => {
   });
 };
 
+/**
+ * 根据预加载文件清单，异步开始请求资源文件
+ * @param {Array} manifest 待处理的Manifest数据集合
+ * @param {Object} options 预处理参数配置选项
+ */
+const loadManifestList = async function(manifest, options = {}) {
+  let result = null;
+  // 判断是否有资源清单的数据集合
+  if (!manifest) {
+    if (api.$cacheManifest) {
+      result = api.$cacheManifest;
+    } else {
+      // 以配置文件形式进行预加载文件转换处理
+      result = await fetchManifestList(options.baseUrl);
+      // 缓存配置文件型的清单列表数据
+      api.$cacheManifest = result;
+    }
+  } else {
+    // 以静态清单数据集合方式进行预加载文件转换处理
+    result = await handleStaticManifestList(manifest);
+  }
+
+  // 预加载完成时的待输出对象
+  const $exports = {};
+  // 过滤筛选已加载完毕的文件资源
+  const manifestList = [];
+  [...result.css, ...result.js].map(item => {
+    const $cache = api.$caches[item.id];
+    // 判断是否未缓存未加载，则放入待预处理队列中
+    if (!$cache || $cache.status !== true) {
+      manifestList.push(item);
+    } else {
+      $cache.exports && Object.assign($exports, $cache.exports);
+    }
+  });
+
+  return new Promise((resolve, reject) => {
+    // 判断是否预加载清单已全部缓存，则直接完成
+    if (!manifestList || !manifestList.length) {
+      resolve($exports);
+    } else {
+      const instance = api.getInstance(options.baseUrl);
+      instance.loadManifest(manifestList);
+      instance.maintainScriptOrder = true;
+      // 当队列遇到任何错误时触发事件
+      const loadError = () => {
+        reject('预加载文件时发生解析错误，请检查文件路径或文件格式是否正确.');
+      };
+      // 触发队列请求中进度情况
+      const progress = response => {
+        const percentage = (response.progress * 100).toFixed(1);
+        // 执行请求进度回调
+        options.onProgress && typeof options.onProgress === 'function' && options.onProgress.call(this, Number(percentage));
+      };
+      // 当队列中单个文件完成加载时触发事件
+      const fileLoaded = response => {
+        const item = response.item;
+        // 记录缓存对象
+        const cache = {
+          id: item.id,
+          src: item.src,
+          type: item.type,
+          tag: response.result,
+          // 文件预加载完成时的状态，当为True时则成功加载，反之则加载失败
+          status: !!(response.result && response.result.tagName && ['LINK', 'SCRIPT'].indexOf(response.result.tagName) !== -1),
+        };
+        if (item.type === 'javascript') {
+          const exportsList = item.exports && Array.isArray(item.exports) ? item.exports : [];
+          const out = {};
+          exportsList.map(key => {
+            if (window[key]) {
+              out[key] = window[key];
+            }
+          });
+          cache.exports = out;
+        } else {
+          cache.exports = null;
+        }
+        // 合并输出对象
+        cache.exports && Object.assign($exports, cache.exports);
+        // 缓存输出的全局对象
+        api.$caches[item.id] = cache;
+        options.onFileload && typeof options.onFileload === 'function' && options.onFileload.call(this, cache);
+      };
+      // 当队列完成加载所有文件时触发事件
+      const loadCompleted = () => {
+        // 注销实例对象
+        instance.destroy();
+        resolve($exports);
+      };
+      // 绑定预加载的回调事件处理
+      instance.on('fileload', fileLoaded, this, false);
+      instance.on('progress', progress, this, false);
+      instance.on('error', loadError, this, false);
+      instance.on('complete', loadCompleted, this, true);
+    }
+  });
+};
+
 const api = {
   // 挂接CreateJs的PreloadJS原生对象
   createjs,
+
+  // 预加载的队列
+  $queue: new PQueue({ concurrency: 1 }),
 
   // 缓存配置文件型Manifest资源清单列表（注意：不会缓存手动加载清单列表）
   $cacheManifest: null,
@@ -154,7 +257,7 @@ const api = {
    * @param {Object} options 选填项，参数配置选项，当单一集合传参或2个传参时，此参数有效.
    * @returns 返回Promise对象
    */
-  load() {
+  async load() {
     let options = {};
     let manifest = null;
     // 判断是否为1个形参数
@@ -173,100 +276,8 @@ const api = {
     // 合并默认参数选项
     const opts = merge({}, DEFAULT_PRELOAD_OPTIONS, options);
 
-    return new Promise((resolve, reject) => {
-      // 预加载文件清单处理完毕
-      const loadFilesReady = data => {
-        // 缓存配置文件型的清单列表数据
-        if (!manifest && !this.$cacheManifest) {
-          this.$cacheManifest = data;
-        }
-        const filesManifest = [...data.css, ...data.js];
-        const outExports = {};
-        // 过滤筛选已加载完毕的文件资源
-        const manifestList = [];
-        filesManifest.map(item => {
-          const cacheItem = this.$caches[item.id];
-          // 判断是否未缓存未加载，则放入待预处理队列中
-          if (!cacheItem || cacheItem.status !== true) {
-            manifestList.push(item);
-          } else {
-            cacheItem.exports && Object.assign(outExports, cacheItem.exports);
-          }
-        });
-
-        // 判断时有待处理的文件资源清单，则进行预处理加载，反之直接完成
-        if (manifestList && manifestList.length) {
-          const instance = this.getInstance(opts.baseUrl);
-          instance.loadManifest(manifestList);
-          instance.maintainScriptOrder = true;
-          // 当队列遇到任何错误时触发事件
-          const loadError = () => {
-            reject('预加载文件时发生解析错误，请检查文件路径或文件格式是否正确.');
-          };
-          // 当队列中单个文件完成加载时触发事件
-          const fileLoaded = response => {
-            const { item, result } = response;
-            // 记录缓存对象
-            const cache = {
-              id: item.id,
-              src: item.src,
-              type: item.type,
-              tag: result,
-              // 文件预加载完成时的状态，当为True时则成功加载，反之则加载失败
-              status: !!(result && result.tagName && ['LINK', 'SCRIPT'].indexOf(result.tagName) !== -1),
-            };
-            if (item.type === 'javascript') {
-              const jsExportsList = item.exports && Array.isArray(item.exports) ? item.exports : [];
-              const jsExports = {};
-              jsExportsList &&
-                jsExportsList.map(key => {
-                  if (window[key]) {
-                    jsExports[key] = window[key];
-                  }
-                });
-              cache.exports = jsExports;
-            } else {
-              cache.exports = null;
-            }
-            // 合并输出对象
-            cache.exports && Object.assign(outExports, cache.exports);
-            // 缓存输出的全局对象
-            this.$caches[item.id] = cache;
-            opts.onFileload && typeof opts.onFileload === 'function' && opts.onFileload.call(this, cache);
-          };
-          // 触发队列请求中进度情况
-          const progress = response => {
-            const percentage = (response.progress * 100).toFixed(1);
-            // 执行请求进度回调
-            opts.onProgress && typeof opts.onProgress === 'function' && opts.onProgress.call(this, Number(percentage));
-          };
-          // 当队列完成加载所有文件时触发事件
-          const loadCompleted = () => {
-            // 注销实例对象
-            instance.destroy();
-            resolve(outExports);
-          };
-          instance.on('fileload', fileLoaded, this, false);
-          instance.on('progress', progress, this, false);
-          instance.on('error', loadError, this, false);
-          instance.on('complete', loadCompleted, this, true);
-        } else {
-          // 当全部已缓存时，则直接完成并输出对象
-          resolve(outExports);
-        }
-      };
-
-      // 判断是否有资源清单的数据集合
-      if (!manifest) {
-        if (this.$cacheManifest) {
-          loadFilesReady(this.$cacheManifest);
-        } else {
-          fetchManifestList(opts.baseUrl).then(loadFilesReady);
-        }
-      } else {
-        handleStaticManifestList(manifest).then(loadFilesReady);
-      }
-    });
+    // 将预加载处理加入队列，防止多load时的并行处理，造成重复加载
+    return this.$queue.add(async () => loadManifestList(manifest, opts));
   },
 
   /**
@@ -287,12 +298,12 @@ const api = {
   getAll() {
     if (this.$caches) {
       const keys = Object.keys(this.$caches);
-      const outExports = {};
+      const $exports = {};
       keys.map(key => {
         const out = this.get(key);
-        out && Object.assign(outExports, out);
+        out && Object.assign($exports, out);
       });
-      return outExports;
+      return $exports;
     }
     return null;
   },
