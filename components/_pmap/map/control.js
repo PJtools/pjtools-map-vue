6b3706ef3e4f7e4c50ff5903e22884176cef0d8d
pjtools <pjtools@vip.qq.com>
2020-05-08 16:12:21 +0800

    :sparkles: 添加地图控件的增删函数方法；

--- components/_pmap/map/control.js
+++ components/_pmap/map/control.js
@@ -13,6 +13,36 @@ const control = {
     const controls = this.vComponent && this.vComponent.$refs.mapControls && this.vComponent.$refs.mapControls.controls;
     return controls && controls[id] ? controls[id].child : null;
   },
+
+  /**
+   * 添加内置地图控件到地图中
+   * @param {String} id 地图控件的Id名
+   * @param {Control} control 地图控件对象
+   */
+  addControl(id, control = {}) {
+    if (id && !this.getMapControl(id)) {
+      control.id = id;
+      this.vComponent.mapControls.push(control);
+    }
+  },
+
+  /**
+   * 根据地图控件Id移除地图控件组件
+   * @param {String} id 地图控件的Id名
+   */
+  removeControl(id) {
+    if (id && this.getMapControl(id)) {
+      const controls = this.vComponent.mapControls;
+      for (let i = controls.length - 1; i >= 0; i--) {
+        if (controls[i].id === id) {
+          this.vComponent.mapControls.splice(i, 1);
+          const vmControls = this.vComponent && this.vComponent.$refs.mapControls && this.vComponent.$refs.mapControls.controls;
+          delete vmControls[id];
+          break;
+        }
+      }
+    }
+  },
 };
 
 export default control;
