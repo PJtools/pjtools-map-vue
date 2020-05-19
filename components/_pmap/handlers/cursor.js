d549345928ac0727b3192dfb43f9ef5c02a86cb0
pjtools <pjtools@vip.qq.com>
2020-05-19 18:55:14 +0800

    :sparkles: 添加Handler.MouseTooltip光标交互组件的实现；

---
+++ components/_pmap/handlers/cursor.js
@@ -0,0 +1,192 @@
+/**
+ * @文件说明: Handlers.Cursor 地图光标
+ * @创建人: pjtools@vip.qq.com
+ * @创建时间: 2020-05-15 13:49:01
+ */
+
+import hat from 'hat';
+import assign from 'lodash/assign';
+import { isEmpty, isBooleanFlase, isString, isFunction } from '../../_util/methods-util';
+
+// 地图光标的默认选项参数
+const defaultCursorOptions = {
+  // 地图光标类型
+  cursor: null,
+  // 是否鼠标跟随提示框
+  tip: false,
+  // 光标跟随提示框的偏移值
+  offset: [0, 0],
+};
+
+// 地图光标跟随提示框的默认选项参数
+const defaultCursorTooltipOptions = {
+  // 样式Class名
+  className: null,
+  // 提示图标
+  icon: null,
+  // 提示内容
+  content: null,
+};
+
+class Cursor {
+  constructor(iMapApi) {
+    this.iMapApi = iMapApi;
+    // 获取地图Canvas容器对象，确定地图光标的范围
+    this._element = iMapApi && iMapApi.getMapCanvasConatainer();
+    // 光标实例对象的唯一Id
+    this._id = `pjmap_interface_mouse_tooltip_${hat()}`;
+    // 光标的参数选项
+    this._options = defaultCursorOptions;
+    // 光标的UI交互组件对象
+    this.iMapApi && this.iMapApi.addMapInterface(this._id, 'MouseTooltip');
+    // 光标的手动显隐状态
+    this._visible = false;
+    // 光标的当前关联Handler/Interfacess实例对象（需实现统一标准：disable 函数方法）
+    this._handler = null;
+    // 光标禁用/启用状态
+    this._enabled = false;
+    // 光标对象的活动状态
+    this._active = false;
+  }
+
+  get component() {
+    if (!this._component) {
+      this._component = this.iMapApi.getMapInterface(this._id);
+    }
+    return this._component;
+  }
+
+  /**
+   * 激活地图光标
+   * @param {Object} options 地图光标的选项
+   * @param {Class} handler 光标关联的交互对象
+   */
+  enable(options = {}, handler = null) {
+    // 设置当前光标模式的选项
+    this._options = assign({}, defaultCursorOptions, options);
+    if (!isBooleanFlase(this._options.tip)) {
+      const tip = isString(this._options.tip) ? { content: this._options.tip } : this._options.tip;
+      this._options.tip = assign({}, defaultCursorTooltipOptions, tip);
+    }
+    // 判断是否有关联交互对象
+    if (this._handler) {
+      this._handler.disable && isFunction(this._handler.disable) && this._handler.disable();
+      this._handler = null;
+    }
+    // 设置当前地图光标模式
+    this._setMapCursorStyle(this._options.cursor);
+    // 判断是否需激活光标提示框组件
+    if (this.component) {
+      this.component.enable(this._options.tip);
+      // 绑定光标的事件监控
+      this.iMapApi.on(`${this._id}.mousemove`, 'mousemove', this._handleMapCursorMouseMove.bind(this));
+      this.iMapApi.on(`${this._id}.mouseover`, 'mouseover', this._handleMapCursorMouseOver.bind(this));
+      this.iMapApi.on(`${this._id}.mouseout`, 'mouseout', this._handleMapCursorMouseOut.bind(this));
+    }
+    handler && (this._handler = handler);
+    // 调整状态
+    this._visible = true;
+    this._enabled = true;
+    this._active = true;
+  }
+
+  /**
+   * 禁用地图光标
+   */
+  disable() {
+    // 移除光标提示框组件
+    if (this.component) {
+      this.component.disable();
+      // 解绑光标的事件
+      this.iMapApi.off(`${this._id}.mousemove`);
+      this.iMapApi.off(`${this._id}.mouseover`);
+      this.iMapApi.off(`${this._id}.mouseout`);
+    }
+    // 清除当前地图光标模式
+    this._setMapCursorStyle(null);
+    // 清除地图光标模式的选项
+    this._options = defaultCursorOptions;
+    // 判断是否有关联交互对象
+    if (this._handler) {
+      this._handler.disable && isFunction(this._handler.disable) && this._handler.disable();
+      this._handler = null;
+    }
+    // 还原状态
+    this._visible = false;
+    this._enabled = false;
+    this._active = false;
+  }
+
+  /**
+   * 获取地图光标当前的禁用/启用状态
+   */
+  isEnabled() {
+    return !!this._enabled;
+  }
+
+  /**
+   * 获取地图光标当前的活动状态
+   */
+  isActive() {
+    return !!this._active;
+  }
+
+  /**
+   * 设置激活地图光标的显隐状态
+   * @param {Boolean} visible 显隐状态
+   */
+  setCursorVisible(visible = true) {
+    if (this._enabled && this._component) {
+      this._component.updateVisible(visible);
+      this._visible = visible;
+    }
+  }
+
+  // 设置地图光标的样式
+  _setMapCursorStyle(cursor) {
+    if (!this._element) {
+      return;
+    }
+    if (cursor) {
+      this._element.style.setProperty('cursor', cursor.trim(), 'important');
+    } else {
+      this._element.style.removeProperty('cursor');
+      if (isEmpty(this._element.style.cssText)) {
+        this._element.removeAttribute('style');
+      }
+    }
+  }
+
+  // 执行地图光标的MouseMove事件
+  _handleMapCursorMouseMove(e) {
+    if (!this._enabled || !this._active || !this._component) {
+      return;
+    }
+    let left = this._options.cursor && this._options.cursor === 'default' ? 18 : 12;
+    let top = this._options.cursor && this._options.cursor === 'default' ? 6 : 4;
+    this._options.offset && this._options.offset[0] && (left += Number(this._options.offset[0]));
+    this._options.offset && this._options.offset[1] && (top += Number(this._options.offset[1]));
+    this._component.updateMousePostion({
+      top: `${e.point.y + top}px`,
+      left: `${e.point.x + left}px`,
+    });
+  }
+
+  // 执行地图光标的MouseOver事件
+  _handleMapCursorMouseOver() {
+    if (this._enabled && this._component) {
+      this._active = true;
+      this._visible && this._component.updateVisible(true);
+    }
+  }
+
+  // 执行地图光标的MouseOut事件
+  _handleMapCursorMouseOut() {
+    if (this._enabled && this._component) {
+      this._active = false;
+      this._visible && this._component.updateVisible(false);
+    }
+  }
+}
+
+export default Cursor;
---
+++ components/map/interfaces/mouse-tooltip/index.jsx
@@ -0,0 +1,146 @@
+/**
+ * @文件说明: Interfaces.MouseTooltip - 光标跟随气泡提示框
+ * @创建人: pjtools@vip.qq.com
+ * @创建时间: 2020-05-18 14:13:35
+ */
+
+import baseProps from '../baseProps';
+import baseMixin from '../baseMixin';
+import { getComponentFromProp } from '../../../_util/antdv';
+import { isEmpty, isFunction, isBooleanTrue } from '../../../_util/methods-util';
+import { Icon } from 'ant-design-vue';
+
+const MouseTooltip = {
+  name: 'PjMap.Interfaces.MouseTooltip',
+  inheritAttrs: false,
+  mixins: [baseMixin],
+  props: baseProps(),
+  data() {
+    return {
+      // 组件是否可见
+      visible: false,
+      // 组件容器Class类名
+      wrapClassName: null,
+      // 提示框的偏移量
+      position: null,
+      // 提示框的显隐状态
+      show: false,
+      // 提示图标
+      icon: null,
+      // 提示内容
+      content: null,
+    };
+  },
+  computed: {
+    // 是否渲染组件
+    isRenderComponent() {
+      return !!(this.visible && (this.icon || this.content));
+    },
+    // 是否为纯图标模式
+    isPureIconMode() {
+      return !!(this.icon && !this.content);
+    },
+    // 组件的样式
+    cptStyles() {
+      const { styles, position, show } = this;
+      const componentStyle = {};
+      styles && Object.assign(componentStyle, styles);
+      position && Object.assign(componentStyle, position);
+      !show && (componentStyle.display = 'none');
+      return componentStyle;
+    },
+    // 组件Class类名
+    classNames() {
+      const {
+        classes,
+        mapProvider: { prefixCls },
+        isPureIconMode,
+      } = this;
+      const cls = [
+        ...classes,
+        `${prefixCls}-interface-mousetooltip`,
+        {
+          'pure-icon': isPureIconMode,
+        },
+      ];
+      this.wrapClassName && cls.push(this.wrapClassName);
+      return cls;
+    },
+  },
+  methods: {
+    // 渲染默认的提示框文本
+    renderTooltipContent(h) {
+      const content = getComponentFromProp(this, 'content', {}, false);
+      if (isFunction(content)) {
+        return <span class="content">{content(h)}</span>;
+      } else {
+        return this.renderSlotScopeNodes(content, this.$data) || <span class="content">{content}</span>;
+      }
+    },
+
+    // 渲染默认的提示框图标
+    renderTooltipIcon(h) {
+      const icon = getComponentFromProp(this, 'icon', {}, false);
+      // 判断是否为Function函数类型
+      if (isFunction(icon)) {
+        return <span class="icon">{icon(h)}</span>;
+      } else {
+        // 判断是否有具名插槽
+        const slotNodes = this.renderSlotScopeNodes(icon, this.$data);
+        if (slotNodes) {
+          return slotNodes;
+        } else {
+          let theme = 'outlined';
+          let iconName = icon;
+          if (icon.indexOf(':') !== -1) {
+            const iconSplit = icon.split(':');
+            theme = iconSplit[1];
+            iconName = iconSplit[0];
+          }
+          return <Icon class="icon" type={iconName} theme={theme} />;
+        }
+      }
+    },
+
+    // 激活光标跟随气泡提示框
+    enable(options = {}) {
+      this.icon = options.icon || null;
+      this.content = options.content || null;
+      this.wrapClassName = options.className || null;
+      this.position = options.position || null;
+      this.show = true;
+      this.visible = true;
+    },
+
+    // 禁用光标跟随气泡提示框
+    disable() {
+      this.wrapClassName = null;
+      this.position = null;
+      this.show = false;
+      this.icon = null;
+      this.content = null;
+      this.visible = false;
+    },
+
+    // 更新光标提示框的偏移量
+    updateMousePostion(position) {
+      this.position = position;
+    },
+
+    // 更新光标提示框的显隐状态
+    updateVisible(show = false) {
+      this.show = isBooleanTrue(show);
+    },
+  },
+  render(h) {
+    const { id, cptStyles, classNames, isRenderComponent, icon, content } = this;
+    return isRenderComponent ? (
+      <div data-id={id} class={classNames} style={cptStyles}>
+        {icon ? this.renderTooltipIcon(h) : null}
+        {content ? this.renderTooltipContent(h) : null}
+      </div>
+    ) : null;
+  },
+};
+
+export default MouseTooltip;
