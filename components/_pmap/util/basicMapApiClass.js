50b9dfa2e0672a68aad0598e454d09012763ebff
pjtools <pjtools@vip.qq.com>
2020-05-21 14:11:06 +0800

    :sparkles: 扩展支持自定义事件驱动的基类；

--- components/_pmap/util/basicMapApiClass.js
+++ components/_pmap/util/basicMapApiClass.js
@@ -22,6 +22,66 @@ const BasicMapApi = (function() {
 
 export default BasicMapApi;
 
+// 扩展带自定义事件的基类
+export const BasicMapApiEvented = (function() {
+  let _evented = Symbol('Evented');
+
+  class BasicMapApiEvented extends BasicMapApi {
+    constructor(iMapApi) {
+      super(iMapApi);
+
+      const { mapboxgl } = iMapApi && iMapApi.exports;
+      this[_evented] = new mapboxgl.Evented();
+    }
+
+    /**
+     * 注册绑定事件
+     * @param {String} type 事件类型名
+     * @param {Function} listener 回调事件函数
+     */
+    on(type, listener) {
+      return this[_evented].on(type, listener);
+    }
+
+    /**
+     * 解除绑定事件
+     * @param {String} type 事件类型名
+     * @param {Function} listener 回调事件函数
+     */
+    off(type, listener) {
+      return this[_evented].off(type, listener);
+    }
+
+    /**
+     * 注册绑定仅只执行一次事件
+     * @param {String} type 事件类型名
+     * @param {Function} listener 回调事件函数
+     */
+    once(type, listener) {
+      return this[_evented].once(type, listener);
+    }
+
+    /**
+     * 驱动执行绑定的事件
+     * @param {Event} event 事件类型名
+     * @param {Object} properties 回调事件函数接收的数据
+     */
+    fire(event, properties) {
+      return this[_evented].fire(event, properties);
+    }
+
+    /**
+     * 判断事件监听是否已注册
+     * @param {String} type 事件类型名
+     */
+    listens(type) {
+      return this[_evented].listens(type);
+    }
+  }
+
+  return BasicMapApiEvented;
+})();
+
 /**
  * 动态扩展Class类的Prototype属性挂接函数方法
  * @param {Object} methods 待动态添加的函数对象
