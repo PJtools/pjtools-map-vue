c48f9af71d14c27ecf4ac0a1355c5c853c3d4135
pjtools <pjtools@vip.qq.com>
2020-05-25 10:45:40 +0800

    :sparkles: 添加CirclePointLayer圆点矢量图层；

---
+++ components/_pmap/layers/vector/circlePointLayer.js
@@ -0,0 +1,61 @@
+/**
+ * @文件说明: Layers.CirclePointLayer 矢量圆点图层
+ * @创建人: pjtools@vip.qq.com
+ * @创建时间: 2020-05-22 18:14:54
+ */
+
+import BasicLayerClass, { defaultVectorLayerOptions, defaultVectorBasicOptions } from './basicLayer';
+import deepmerge from 'deepmerge';
+
+const defaultLayerOptions = deepmerge(defaultVectorLayerOptions, {
+  paint: {
+    'circle-color': 'rgba(255, 216, 191, 0.9)',
+    'circle-opacity': 1,
+    'circle-radius': 5,
+    'circle-blur': 0,
+    'circle-stroke-color': 'rgba(250, 84, 28, 0.9)',
+    'circle-stroke-opacity': 1,
+    'circle-stroke-width': 2,
+  },
+});
+
+const defaultOptions = {
+  ...defaultVectorBasicOptions,
+};
+
+class CirclePointLayer extends BasicLayerClass {
+  constructor(iMapApi, id, layerOptions = {}, options = {}) {
+    const opts = deepmerge.all([{}, defaultOptions, options]);
+    opts.opacityPaints = ['circle-opacity', 'circle-stroke-opacity'];
+    // 合并原生Layer图层属性
+    const layer = deepmerge.all([{}, defaultLayerOptions, layerOptions]);
+    layer.type = 'circle';
+    // 继承矢量图层基类
+    super(iMapApi, id, layer, opts);
+  }
+
+  /**
+   * 设置图层的整体透明度
+   * @param {Number} opacity 待设定的透明度
+   */
+  setOpacity(opacity) {
+    super.setOpacity(opacity, ['circle-opacity', 'circle-stroke-opacity']);
+  }
+
+  /**
+   * 获取圆点的半径大小
+   */
+  getRadius() {
+    return this.getPaint('circle-radius');
+  }
+
+  /**
+   * 设置圆点的半径大小
+   * @param {Number} radius 圆点的半径大小
+   */
+  setRadius(radius) {
+    radius && this.setPaint('circle-radius', radius);
+  }
+}
+
+export default CirclePointLayer;
