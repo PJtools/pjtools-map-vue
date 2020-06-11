efe9a3e92a66bd008d13b8d5d11b99207dc0fb80
pjtools <pjtools@vip.qq.com>
2020-06-11 18:46:34 +0800

    :sparkles: 添加Ant Design Vue的工具类；

--- components/_util/antdv.js
+++ components/_util/antdv.js
@@ -8,5 +8,6 @@ import antDirective from 'ant-design-vue/es/_util/antDirective';
 import PropTypes from 'ant-design-vue/es/_util/vue-types';
 import { initDefaultProps, filterEmpty, getComponentFromProp } from 'ant-design-vue/es/_util/props-util';
 import getTransitionProps from 'ant-design-vue/es/_util/getTransitionProps';
+import BaseMixin from 'ant-design-vue/es/_util/BaseMixin';
 
-export { antDirective, PropTypes, initDefaultProps, filterEmpty, getComponentFromProp, getTransitionProps };
+export { antDirective, PropTypes, initDefaultProps, filterEmpty, getComponentFromProp, getTransitionProps, BaseMixin };
