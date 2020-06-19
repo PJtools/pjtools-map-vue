c9dccb04dfe65e31783ead73789216a5a5026fc2
pjtools <pjtools@vip.qq.com>
2020-06-19 10:04:32 +0800

    :memo: 添加Google谷歌地图服务源示例DEMO；

---
+++ components/map/demo/examples/Providers/Google/basic.md
@@ -0,0 +1,16 @@
+<cn>
+#### 默认Google地图
+通过配置文件初始化渲染Google谷歌地图服务源。
+</cn>
+
+<us>
+#### basic
+xx.
+</us>
+
+```tpl
+<template>
+	<pj-map bordered height="480" config="../MapConfig/examples/google.json">
+	</pj-map>
+</template>
+```
---
+++ components/map/demo/examples/Providers/Google/index.vue
@@ -0,0 +1,27 @@
+<script>
+const md = {
+	cn: `
+		# Google 谷歌地图
+		地图采用Google谷歌地图作为初始底图资源。底图服务源是以地图配置文件形式设定，具体API说明请查阅 [Map](/components/map-cn/) 地图示例。
+
+		> **投影坐标**： GCJ02火星坐标
+
+		## 代码演示
+	`,
+};
+
+import Basic from './basic';
+
+export default {
+	title: 'Google',
+	subtitle: 'Google谷歌地图',
+	render() {
+		return (
+			<div>
+				<md cn={md.cn} />
+				<Basic />
+			</div>
+		)
+	},
+};
+</script>
