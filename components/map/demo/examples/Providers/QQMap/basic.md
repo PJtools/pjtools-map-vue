81e0bb51d7064682d24ac7b0944a774ccec65549
pjtools <pjtools@vip.qq.com>
2020-06-19 10:19:46 +0800

    :memo: 添加腾讯地图服务源示例DEMO；

---
+++ components/map/demo/examples/Providers/QQMap/basic.md
@@ -0,0 +1,16 @@
+<cn>
+#### 默认腾讯地图
+通过配置文件初始化渲染腾讯地图服务源。
+</cn>
+
+<us>
+#### basic
+xx.
+</us>
+
+```tpl
+<template>
+	<pj-map bordered height="480" config="../MapConfig/examples/qqmap.json">
+	</pj-map>
+</template>
+```
---
+++ components/map/demo/examples/Providers/QQMap/index.vue
@@ -0,0 +1,27 @@
+<script>
+const md = {
+	cn: `
+		# QQMap 腾讯地图
+		地图采用腾讯地图作为初始底图资源。底图服务源是以地图配置文件形式设定，具体API说明请查阅 [Map](/components/map-cn/) 地图示例。
+
+		> **投影坐标**： GCJ02火星坐标
+
+		## 代码演示
+	`,
+};
+
+import Basic from './basic';
+
+export default {
+	title: 'QQMap',
+	subtitle: '腾讯地图',
+	render() {
+		return (
+			<div>
+				<md cn={md.cn} />
+				<Basic />
+			</div>
+		)
+	},
+};
+</script>
